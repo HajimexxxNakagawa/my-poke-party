30ef3b6ab1c49d988c913f4a095ddf36e179482d
Hajime Nakagawa <navyblue0914@icloud.com>
2022-02-13 13:55:10 +0900

    set og image

--- src/components/page/Top/Page.tsx
+++ src/components/page/Top/Page.tsx
@@ -8,12 +8,15 @@ export const TopPage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>NextWind</title>
+        <title>シンオウで育った同士へ</title>
         <meta
           name="description"
-          content="A template for prototyping with Next.js and Tailwind CSS"
+          content="聞こえてくる... おんみょ〜んの鐘の音が"
         />
         <link rel="icon" href="/favicon.ico" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:image" content="/og.png" />
       </Head>
 
       <TopPageView />
